e56dba32ff9c42f79c4f3ac1bb388afaa18c717e
jackson <2121354@jeffcoschools.us>
2025-08-29 10:18:52 -0600

    assignment

---
+++ burnbot.py
@@ -0,0 +1,10 @@
+print("burnbot")
+
+age = input("how old are you")
+sport = input("whats your favorite sport")
+name = ("whats your name")
+car = ("whats your favorite car")
+family = ("how many members in your family")
+activities = ("what activities do you do")
+
+print(f"hi{name}, you must think your all that being {age}, I havnt existed for very long and i know that {sport} and {car} are such original choices. You literally have {family} members in your family and none of them like you. Plus you do {activities}, which makes oyu even lamer")
